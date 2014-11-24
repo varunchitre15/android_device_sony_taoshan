75485247ddc8a7eb38d1dedb41609cf8b3ed122f
Varun Chitre <varun.chitre15@gmail.com>
2014-11-24 16:37:33 +0100

    copy the logo again
    Change-Id: Ifde3c39eba598ec6b1221b3a52c847715f6ded8a

--- custombootimg.mk
+++ custombootimg.mk
@@ -5,6 +5,7 @@ $(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
 	zcat $< > $@
 
 INITSH := device/sony/taoshan/combinedroot/init.sh
+LOGO := device/sony/taoshan/rootdir/root/logo.rle
 BOOTREC_DEVICE := $(LOCAL_PATH)/recovery/root/etc/bootrec-device
 
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
@@ -21,6 +22,7 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(uncompressed_ramdisk) $(r
 	$(hide) cp $(PRODUCT_OUT)/utilities/extract_elf_ramdisk $(PRODUCT_OUT)/combinedroot/sbin/
 
 	$(hide) cp $(INITSH) $(PRODUCT_OUT)/combinedroot/sbin/init.sh
+	$(hide) cp $(LOGO) $(PRODUCT_OUT)/combinedroot/logo.rle
 	$(hide) chmod 755 $(PRODUCT_OUT)/combinedroot/sbin/init.sh
 	$(hide) ln -s sbin/init.sh $(PRODUCT_OUT)/combinedroot/init
 	$(hide) cp $(BOOTREC_DEVICE) $(PRODUCT_OUT)/combinedroot/sbin/
